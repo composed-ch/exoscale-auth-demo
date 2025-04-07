c029dd63d91dce9a5661dc64e5d352cee6f291d5
Patrick Bucher <patrick.bucher@mailbox.org>
2025-04-07 18:00:32 +0200

    example post request

--- src/exoscale_auth_demo/core.clj
+++ src/exoscale_auth_demo/core.clj
@@ -2,6 +2,7 @@
   (:require [exoscale-auth.core :as exo])
   (:require [clj-http.client :as client])
   (:require [clojure.edn :as edn])
+  (:require [clojure.data.json :as json])
   (:gen-class))
 
 (defn read-config [path]
@@ -17,13 +18,29 @@
         api-key (:exoscale-api-key conf)
         api-secret (:exoscale-api-secret conf)
         headers {:content-type "application/json"}
-        auth-header (exo/build-auth-header api-key api-secret "GET" resource {} query headers)
+        auth-header (exo/build-auth-header api-key api-secret "GET" resource {} query)
         headers (assoc headers :Authorization auth-header)
-        url-func (exo-base-url-func {:exoscale-zone "ch-gva-2"})
+        url-func (exo-base-url-func conf)
         url (url-func resource)]
     (client/get url {:headers headers})))
+
+(defn exo-do [method resource query body]
+  (let [conf (read-config "conf.edn")
+        api-key (:exoscale-api-key conf)
+        api-secret (:exoscale-api-secret conf)
+        headers {:content-type "application/json"}
+        auth-header (exo/build-auth-header api-key api-secret method resource body query)
+        headers (assoc headers :Authorization auth-header)
+        url-func  (exo-base-url-func conf)
+        url (url-func resource)
+        func (case (.toLowerCase method)
+               "get" client/get
+               "post" client/post
+               (fn [& args] "method %s not implemented" method))]
+    (func url {:headers headers :body (json/write-str body)})))
+
                    
 (defn -main
   [& args]
   (let [conf (read-config "conf.edn")]
-    (println (exo-get "v2/instance/25fd0b49-be79-44a4-81b8-282d1d609b8e" {}))))
+    (println (exo-do "POST" "v2/security-group" {} {:name "sikjurity" :description "top sikret"}))))
